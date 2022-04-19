f332fe3c46eb0b3db0ed368ab828a336bb6aedc8
misheck12 <misheck1720@gmail.com>
2022-04-19 17:46:51 +0200

    replace functions with classes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
-}
+const App = () => (
+  <Calculator />
+);
 
 export default App;
