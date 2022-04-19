035ebc1c038102df3258af59f3c1cd6fcd6ea679
misheck12 <misheck1720@gmail.com>
2022-04-19 23:12:20 +0200

    modify button functionalities

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,20 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Button extends React.PureComponent {
-  render() {
-    const { ctrls, last } = this.props;
+  render = () => {
+    const { ctrls, last, setBtn } = this.props;
     const listItems = ctrls.map((ctrl, index) => {
       let cl = 'bg-ash';
       let row = '';
       let selectLast = 3;
-      if (last && index === 0) {
-        row = 'ctrl-btn-2-row';
+      if (last) {
+        if (index === 0) {
+          row = 'ctrl-btn-2-row';
+        }
         selectLast = 2;
       }
       if (index === selectLast) {
         cl = 'bg-orange';
       }
-      return <button key={ctrl} type="button" className={`ctrl-btn-row ${row} ${cl} font`}>{ctrl}</button>;
+      return <button key={ctrl} type="button" onClick={() => setBtn(ctrl)} className={`ctrl-btn-row ${row} ${cl} font`}>{ctrl}</button>;
     }, last);
     return (
       <div className="ctrls">{listItems}</div>
@@ -25,6 +27,7 @@ class Button extends React.PureComponent {
 
 Button.propTypes = {
   ctrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+  setBtn: PropTypes.func.isRequired,
   last: PropTypes.bool.isRequired,
 };
 
